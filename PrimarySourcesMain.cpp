9a8e3c73263cc29f20ee063649bbacc346af7c1f
NullifiedV <31392897+NullifiedV@users.noreply.github.com>
2017-09-21 08:55:40 -0700

    Added PrimarySourcesMain.cpp
    Added PrimarySourcesMain. Not complete, but still leads to webpage

---
+++ PrimarySourcesMain.cpp
@@ -0,0 +1,48 @@
+// Primary Sources
+
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+using namespace std;
+
+// Code Snippet 1
+
+#define string
+#define m
+#define f
+#define wait
+
+void wait();
+
+}
+
+void pushstring();
+cout << "Welcome to Tai's Primary Source db " << cout
+printf("This database includes all the images and poster design needed for the project");
+cout << "Loading..." << cout
+wait(text>-> "Loading n\ Please wait"));
+
+// Code snippet 2 <after load>
+
+#define >OK<
+
+<OK>(messagebox1(A_MESSAGEBOX_STRING("Click YES for more information. Click NO to close PrimarySource.git.((yes\n No))));
+
+}
+
+//CODE SNIPPET 3
+
+if input=yes then use string
+
+#define string
+
+int; string
+
+
+
+
+
+
+
+  
